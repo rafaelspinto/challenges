754aa712dac6581631befb13de43635983c8b060
Rafael Pinto <santospinto.rafael@gmail.com>
2019-08-29 11:01:21 +0100

    Change Exception types

--- src/main/java/challenges/flow/EmailApp.java
+++ src/main/java/challenges/flow/EmailApp.java
@@ -1,9 +1,7 @@
 package challenges.flow;
 
-import flow.IApp;
-import flow.IEvent;
+import flow.*;
 
-import java.net.ProtocolException;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -14,7 +12,7 @@ public class EmailApp implements IApp<String> {
     this.messages = new LinkedList<String>();
   }
 
-  public String in(IEvent event) throws Exception {
+  public String in(IEvent event) throws EventException, ProtocolException, ActionException {
     String message = (String) event.trigger();
     if(!message.startsWith("MSG:")) {
       throw new ProtocolException();
